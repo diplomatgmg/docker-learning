65515f70a62eab100cd6a7bfc9731795abf7ff71
Кирилл Попов <diplomatgmg@gmail.com>
2023-04-11 23:53:24 +0300

    Оптимизированы запросы в БД

--- service/apps/services/serializers.py
+++ service/apps/services/serializers.py
@@ -6,15 +6,23 @@ from apps.services.models import Subscription, Plan
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
-        fields = '__all__'
+        fields = "__all__"
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
-    plan = PlanSerializer()
-    client_name = serializers.CharField(source='client.company_name')
-
-    email = serializers.EmailField(source='client.user.email')
-
     class Meta:
         model = Subscription
-        fields = ('plan', 'client_name', 'email')
+        fields = ("plan", "client_name", "email", "price")
+
+    plan = PlanSerializer()
+    client_name = serializers.CharField(source="client.company_name")
+
+    email = serializers.EmailField(source="client.user.email")
+    price = serializers.SerializerMethodField()
+
+    # def get_price(self, instance: Subscription):
+    #     full_price = instance.service.full_price
+    #     return full_price - full_price * (instance.plan.discount_percent / 100)
+
+    def get_price(self, instance: Subscription):
+        return instance.price
--- service/apps/services/views.py
+++ service/apps/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from apps.clients.models import Client
@@ -10,11 +10,30 @@ class SubscriptionView(ReadOnlyModelViewSet):
     serializer_class = SubscriptionSerializer
 
     queryset = Subscription.objects.prefetch_related(
-        'plan',
-        Prefetch('client',
-                 queryset=Client.objects.select_related('user').only('company_name',
-                                                                     'user__email'))
+        "plan",
+        Prefetch(
+            "client",
+            queryset=Client.objects.select_related("user").only(
+                "company_name", "user__email"
+            ),
+        ),
+    ).annotate(
+        price=F("service__full_price")
+        - (F("service__full_price") * F("plan__discount_percent") / 100.0)
     )
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {
+            "result": response.data,
+            "total_price": self.queryset.aggregate(total_price=Sum("price")).get(
+                "total_price"
+            ),
+        }
+        response.data = response_data
+
+        return response
 
 
 class PlanView(ReadOnlyModelViewSet):
